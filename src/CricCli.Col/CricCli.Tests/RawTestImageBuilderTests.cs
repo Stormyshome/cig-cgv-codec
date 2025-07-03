52577894810cb4d408414f81cdd217acd6d3aaed
Christian Schuetz <Christian.Schuetz1@outlook.com>
2025-07-04 01:06:42 +0200

    added test Project

---
+++ src/CricCli.Col/CricCli.Tests/RawTestImageBuilderTests.cs
@@ -0,0 +1,28 @@
+using Xunit;
+using System.IO;
+
+namespace CricCli.Tests
+{
+    public class RawTestImageBuilderTests
+    {
+        [Fact]
+        public void CreatesWhiteImageCorrectly()
+        {
+            string path = "test_white.raw";
+            int width = 4;
+            int height = 4;
+
+            RawTestImageBuilder.CreateTestImage(path, width, height, ImageFormat.RawGray8, "white");
+
+            byte[] data = File.ReadAllBytes(path);
+            Assert.Equal(width * height, data.Length);
+
+            foreach (byte b in data)
+            {
+                Assert.Equal(255, b);
+            }
+
+            File.Delete(path);
+        }
+    }
+}
